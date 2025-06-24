ff362451b2d9717dfe8e2f85a0ae4ffe4b8e73b2
Chetan Goel <chetan.goel2019@gmail.com>
2025-06-24 13:07:32 -0400

    postgres db setup

--- mappa-backend/config.py
+++ mappa-backend/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', '***REMOVED***')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///mappa.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret-key')
